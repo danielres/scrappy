93903e28cf941009e4a2c839d8bb643552a160cd
Daniel Reszka <dannn.r@gmail.com>
2025-04-12 10:25:42 +0200

    sanitizeFileName: add test

---
+++ src/utils/sanitizeFilename.test.ts
@@ -0,0 +1,11 @@
+import test from 'node:test'
+import assert from 'node:assert'
+import { sanitizeFilename } from './sanitizeFilename.ts'
+
+test('sanitizeFilename', async (t) => {
+  t.test('handles diacritics', () => {
+    const actual = sanitizeFilename('À la plage en été')
+    const expected = 'A la plage en ete'
+    assert.equal(actual, expected)
+  })
+})
