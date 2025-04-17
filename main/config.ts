d64a9c3ead21b3e97296360b01dc23af1ebd224a
Daniel Reszka <dannn.r@gmail.com>
2025-04-17 21:27:41 +0200

    config: add OPENAI_MODEL

--- main/config.ts
+++ main/config.ts
@@ -1,5 +1,6 @@
 export default {
   OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+  OPENAI_MODEL: "gpt-4.1-nano",
   TEMPLATES_DIR: 'main/templates/',
   EMAIL_TEMPLATE: 'email.md',
   XLSX_IN: 'main/organizations.xlsx',
