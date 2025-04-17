38fa9da95743b3085de8e72ea23e0f92b66e94c6
Daniel Reszka <dannn.r@gmail.com>
2025-04-17 10:41:41 +0200

    extract generateTypes to bin/

--- src/scrappy.ts
+++ src/scrappy.ts
@@ -1,11 +1,9 @@
 import config from '../config.ts'
-import { generateTypes } from './utils/dev.ts'
 import row from './utils/row.ts'
 import Workbook from './Workbook.ts'
 
 const wb = new Workbook(config.XLSX_IN)
 
-generateTypes(wb)
 await wb.processEachRowWith(row.scrapeOrgData, { skippable: true })
 await wb.processEachRowWith(row.genMessage, { skippable: true })
 
